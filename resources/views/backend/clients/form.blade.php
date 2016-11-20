b893a86b1ad372f88648bafd7c2d64e39d3bb504
Reza Esmaili <github@dfox.info>
2016-11-20 12:24:32 +0100

    Adjust client form

--- resources/views/backend/clients/form.blade.php
+++ resources/views/backend/clients/form.blade.php
@@ -20,41 +20,6 @@
     <div class="box-body">
         {!! form_until($form, 'website') !!}
     </div>
-</div>
-<div class="box box-primary">
-    <div class="box-header with-border">
-        <h3 class="box-title">{{ trans('backend/client_profiles.push_notification_server') }}</h3>
-    </div>
-    <div class="box-body">
-        {!! form_row($form->profile->pns_app_id) !!}
-        {!! form_row($form->profile->pns_api_key) !!}
-    </div>
-</div>
-<div class="box box-primary">
-    <div class="box-header with-border">
-        <h3 class="box-title">{{ trans('backend/client_profiles.app_store_info') }}</h3>
-    </div>
-    <div class="box-body">
-        {!! form_row($form->profile->apple_app_id) !!}
-        {!! form_row($form->profile->android_app_id) !!}
-    </div>
-</div>
-<div class="box box-primary">
-    <div class="box-header with-border">
-        <h3 class="box-title">{{ trans('backend/client_profiles.project_info') }}</h3>
-    </div>
-    <div class="box-body">
-        {!! form_row($form->profile->client_number) !!}
-        {!! form_row($form->profile->opt_in_email_template_id) !!}
-        {!! form_row($form->profile->password_forgotten_email_template_id) !!}
-        {!! form_row($form->profile->contact_email_template_id) !!}
-        {!! form_row($form->profile->personal_shopping_email_template_id) !!}
-        {!! form_row($form->profile->invitation_email_template_id) !!}
-        {!! form_row($form->profile->code_type) !!}
-        {!! form_row($form->profile->footer) !!}
-        {!! form_row($form->profile->logo) !!}
-        {!! form_row($form->profile->app_logo) !!}
-    </div>
     <div class="box-footer">
         {!! form_row($form->submit) !!}
     </div>
