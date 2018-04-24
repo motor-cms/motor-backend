2c25c49f45e5b988ed3350f58cb8a269a485c110
Reza Esmaili <github@dfox.info>
2018-04-24 16:15:49 +0200

    Update flash message code

--- resources/views/layouts/partials/message.blade.php
+++ resources/views/layouts/partials/message.blade.php
@@ -1,52 +1,57 @@
-@if (session()->has('flash_notification.message'))
-    @if (session()->has('flash_notification.overlay'))
+@foreach (session('flash_notification', collect())->toArray() as $message)
+    @if ($message['overlay'])
         @include('flash::modal', [
             'modalClass' => 'flash-modal',
-            'title'      => session('flash_notification.title'),
-            'body'       => session('flash_notification.message')
+            'title'      => $message['title'],
+            'body'       => $message['message']
         ])
     @else
+
         @if (config('motor-backend.flash') == 'alert')
-        <div class="flash-message">
-            <p>
-            <div class="alert
-                    alert-{{ session('flash_notification.level') }}
-            {{ session()->has('flash_notification.important') ? 'alert-important' : '' }}"
-            >
-                @if(session()->has('flash_notification.important'))
-                    <button type="button"
-                            class="close"
-                            data-dismiss="alert"
-                            aria-hidden="true"
-                    >&times;</button>
-                @endif
+            <div class="flash-message">
+                <p>
+                <div class="alert
+                    alert-{{ $message['level'] }}
+                {{ $message['important'] ? 'alert-important' : '' }}"
+                     role="alert"
+                >
+                    @if ($message['important'])
+                        <button type="button"
+                                class="close"
+                                data-dismiss="alert"
+                                aria-hidden="true"
+                        >&times;
+                        </button>
+                    @endif
 
-                {!! session('flash_notification.message') !!}
+                    {!! $message['message'] !!}
+                </div>
+                </p>
             </div>
-            </p>
-        </div>
         @elseif(config('motor-backend.flash') == 'toastr')
-        @section('view_scripts')
-            <script type="text/javascript">
-                var notificationLevel = '{{ session('flash_notification.level') }}';
-                toastr.options = {progressBar: true};
+@section('view_scripts')
+    <script type="text/javascript">
+        var notificationLevel = '{{ $message['level'] }}';
+        toastr.options = {progressBar: true};
 
-                switch (notificationLevel) {
-                    case 'success':
-                        toastr.success('{!! session('flash_notification.message') !!}', '{{ trans('motor-backend::backend/global.flash.success') }}');
-                        break;
-                    case 'error':
-                        toastr.error('{!! session('flash_notification.message') !!}', '{{ trans('motor-backend::backend/global.flash.error') }}');
-                        break;
-                    case 'warning':
-                        toastr.warning('{!! session('flash_notification.message') !!}', '{{ trans('motor-backend::backend/global.flash.warning') }}');
-                        break;
-                    case 'info':
-                        toastr.info('{!! session('flash_notification.message') !!}', '{{ trans('motor-backend::backend/global.flash.info') }}');
-                        break;
-                }
-            </script>
-        @append
-        @endif
-    @endif
+        switch (notificationLevel) {
+            case 'success':
+                toastr.success('{!! $message['message'] !!}', '{{ trans('motor-backend::backend/global.flash.success') }}');
+                break;
+            case 'error':
+                toastr.error('{!! $message['message'] !!}', '{{ trans('motor-backend::backend/global.flash.error') }}');
+                break;
+            case 'warning':
+                toastr.warning('{!! $message['message'] !!}', '{{ trans('motor-backend::backend/global.flash.warning') }}');
+                break;
+            case 'info':
+                toastr.info('{!! $message['message'] !!}', '{{ trans('motor-backend::backend/global.flash.info') }}');
+                break;
+        }
+    </script>
+@append
 @endif
+@endif
+@endforeach
+
+{{ session()->forget('flash_notification') }}
