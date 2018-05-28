88412dc460ab56493c1138b97b4a768dc304be51
Reza Esmaili <github@dfox.info>
2018-05-29 01:29:12 +0200

    Add relative path to original uploaded file

--- src/Helpers/MediaHelper.php
+++ src/Helpers/MediaHelper.php
@@ -11,13 +11,17 @@ class MediaHelper
     {
         $data  = [];
         $items = $record->getMedia($identifier);
+
+        $host = ( isset($_SERVER['HTTPS']) ? "https" : "http" ) . "://$_SERVER[HTTP_HOST]";
+
         if (isset($items[0])) {
-            $data['file_original'] = asset($items[0]->getUrl());
-            $data['file_size']     = $items[0]->size;
-            $data['name']          = $items[0]->name;
-            $data['file_name']     = $items[0]->file_name;
-            $data['mime_type']     = \GuzzleHttp\Psr7\mimetype_from_filename($items[0]->file_name);
-            $data['is_generating'] = $items[0]->hasCustomProperty('generating');
+            $data['file_original']          = asset($items[0]->getUrl());
+            $data['file_original_relative'] = str_replace($host, '', asset($items[0]->getUrl()));
+            $data['file_size']              = $items[0]->size;
+            $data['name']                   = $items[0]->name;
+            $data['file_name']              = $items[0]->file_name;
+            $data['mime_type']              = \GuzzleHttp\Psr7\mimetype_from_filename($items[0]->file_name);
+            $data['is_generating']          = $items[0]->hasCustomProperty('generating');
 
             if ($base64) {
                 $data['file_base64'] = base64_encode(file_get_contents(public_path() . urldecode($items[0]->getUrl())));
