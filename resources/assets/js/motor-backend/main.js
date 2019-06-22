ee177bfa6e1b4bea326aca31572ffdca9e721d42
Reza Esmaili <bitbucket@dfox.info>
2019-06-22 20:35:59 +0200

    Add wordwrap mixin; Add extra large modal css

--- resources/assets/js/motor-backend/main.js
+++ resources/assets/js/motor-backend/main.js
@@ -86,3 +86,20 @@ Vue.component(
     'motor-backend-file-association-field',
     require('./components/fields/FileAssociationField.vue').default
 );
+
+
+Vue.mixin({
+    methods: {
+        wordwrap: function( str, width, brk, cut ) {
+            brk = brk || 'n';
+            width = width || 75;
+            cut = cut || false;
+
+            if (!str) { return str; }
+
+            var regex = '.{1,' +width+ '}(\s|$)' + (cut ? '|.{' +width+ '}|.+$' : '|\S+?(\s|$)');
+
+            return str.match( RegExp(regex, 'g') ).join( brk );
+        }
+    }
+});
