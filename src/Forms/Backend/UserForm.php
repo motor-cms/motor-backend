ad3d22ed05a186ad4c51e519f6f9498726108da0
Reza Esmaili <bitbucket@dfox.info>
2019-11-17 21:36:59 +0100

    Update UserService and UserForm to allow users with reduced rights to add users with their respective rights

--- src/Forms/Backend/UserForm.php
+++ src/Forms/Backend/UserForm.php
@@ -3,6 +3,7 @@
 namespace Motor\Backend\Forms\Backend;
 
 use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Support\Facades\Auth;
 use Motor\Backend\Models\Role;
 use Kris\LaravelFormBuilder\Form;
 use Motor\Backend\Models\User;
@@ -21,42 +22,55 @@ class UserForm extends Form
      */
     public function buildForm()
     {
-        $selected = [];
+        $selectedRoles = [];
+        $roles = Role::pluck('name', 'id')->toArray();
+
         if (is_object($this->model) && $this->model->roles instanceof Collection) {
             foreach ($this->model->roles as $role) {
-                $selected[] = $role->id;
+                $selectedRoles[] = $role->id;
             }
         }
 
-        $clients = config('motor-backend.models.client')::pluck('name', 'id')->toArray();
-        $this->add('client_id', 'select', [
+        if (Auth::user()->client_id > 0) {
+            $roles = Auth::user()->roles()->pluck('name', 'id')->toArray();
+        }
+
+        if (is_null(Auth::user()->client_id)) {
+            $this->add('client_id', 'select', [
                 'label'       => trans('motor-backend::backend/clients.client'),
-                'choices'     => $clients,
+                'choices'     => config('motor-backend.models.client')::orderBy('name', 'asc')->pluck('name',
+                    'id')->toArray(),
                 'empty_value' => trans('motor-backend::backend/global.all')
-            ])
-             ->add('name', 'text', [ 'label' => trans('motor-backend::backend/users.name'), 'rules' => 'required' ])
-             ->add('email', 'text', [ 'label' => trans('motor-backend::backend/users.email'), 'rules' => 'required' ])
-             ->add('password', 'password', [ 'value' => '', 'label' => trans('motor-backend::backend/users.password') ])
-             ->add(
-                 'avatar',
-                 'file_image',
-                 [ 'label' => trans('motor-backend::backend/global.image'), 'model' => User::class ]
-             )
-             ->add('roles', 'choice', [
-                 'label'          => trans('motor-backend::backend/roles.roles'),
-                 'choice_options' => [
-                     'wrapper'    => [ 'class' => 'choice-wrapper' ],
-                     'label_attr' => [ 'class' => 'label-class' ],
-                 ],
-                 'selected'       => $selected,
-                 'expanded'       => true,
-                 'multiple'       => true,
-                 'choices'        => Role::pluck('name', 'id')->toArray(),
-             ])
-             ->add(
-                 'submit',
-                 'submit',
-                 [ 'attr' => [ 'class' => 'btn btn-primary' ], 'label' => trans('motor-backend::backend/users.save') ]
-             );
+            ]);
+        }
+
+        $this->add('name', 'text', [ 'label' => trans('motor-backend::backend/users.name'), 'rules' => 'required' ])
+            ->add('email', 'text', [ 'label' => trans('motor-backend::backend/users.email'), 'rules' => 'required' ])
+            ->add('password', 'password', [ 'value' => '', 'label' => trans('motor-backend::backend/users.password') ])
+            ->add(
+                'avatar',
+                'file_image',
+                [ 'label' => trans('motor-backend::backend/global.image'), 'model' => User::class ]
+            );
+
+        if (count($roles)) {
+            $this->add('roles', 'choice', [
+                'label'          => trans('motor-backend::backend/roles.roles'),
+                'choice_options' => [
+                    'wrapper'    => [ 'class' => 'choice-wrapper' ],
+                    'label_attr' => [ 'class' => 'label-class' ],
+                ],
+                'selected'       => $selectedRoles,
+                'expanded'       => true,
+                'multiple'       => true,
+                'choices'        => $roles,
+            ]);
+        }
+
+        $this->add(
+            'submit',
+            'submit',
+            [ 'attr' => [ 'class' => 'btn btn-primary' ], 'label' => trans('motor-backend::backend/users.save') ]
+        );
     }
 }
--- src/Services/UserService.php
+++ src/Services/UserService.php
@@ -3,9 +3,8 @@
 namespace Motor\Backend\Services;
 
 use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Str;
-use Motor\Backend\Models\Permission;
-use Motor\Backend\Models\Role;
 use Motor\Backend\Models\User;
 
 /**
@@ -25,24 +24,20 @@ class UserService extends BaseService
 
     public function beforeCreate()
     {
-        $this->data['password']  = bcrypt($this->data['password']);
+        if (Auth::user()->client_id > 0) {
+            $this->record->client_id = Auth::user()->client_id;
+        }
+
         $this->data['api_token'] = Str::random(60);
+
+        $this->updatePassword();
     }
 
 
     public function afterCreate()
     {
-        if (has_permission('roles.write')) {
-            foreach (Arr::get($this->data, 'roles', []) as $role) {
-                $this->record->assignRole($role);
-            }
-        }
-        if (has_permission('permissions.write')) {
-            foreach (Arr::get($this->data, 'permissions', []) as $permission) {
-                $this->record->givePermissionTo(Permission::find((int) $permission));
-            }
-        }
-        $this->uploadFile($this->request->file('avatar'), 'avatar');
+        $this->syncRolesAndPermissions();
+        $this->uploadFiles();
     }
 
 
@@ -52,7 +47,17 @@ class UserService extends BaseService
         if (Arr::get($this->data, 'api_token')) {
             unset($this->data['api_token']);
         }
+        $this->updatePassword();
+    }
 
+    public function afterUpdate()
+    {
+        $this->syncRolesAndPermissions();
+        $this->uploadFiles();
+    }
+
+    private function updatePassword()
+    {
         if (Arr::get($this->data, 'password') == '') {
             unset($this->data['password']);
         } else {
@@ -60,25 +65,16 @@ class UserService extends BaseService
         }
     }
 
-
-    public function afterUpdate()
+    private function uploadFiles()
     {
-        if (has_permission('roles.write')) {
-            if (is_array(Arr::get($this->data, 'roles'))) {
-                foreach (Role::all() as $role) {
-                    $this->record->removeRole($role);
-                }
-            }
-        }
+        $this->uploadFile($this->request->file('avatar'), 'avatar');
+    }
 
-        if (has_permission('permissions.write')) {
-            if (is_array(Arr::get($this->data, 'permissions'))) {
-                foreach (Permission::all() as $permission) {
-                    $this->record->revokePermissionTo($permission);
-                }
-            }
-        }
+    private function syncRolesAndPermissions()
+    {
+        $this->record->roles()->sync(Arr::get($this->data, 'roles', []));
 
-        $this->afterCreate();
+
+        $this->record->permissions()->sync(Arr::get($this->data, 'permissions', []));
     }
 }
