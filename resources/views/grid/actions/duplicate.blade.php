b9709b3690b6d3020e56f172c49762c0fc90aee5
Reza Esmaili <bitbucket@dfox.info>
2019-11-23 16:31:18 +0100

    Add type and record id to button data attributes

--- resources/views/grid/actions/duplicate.blade.php
+++ resources/views/grid/actions/duplicate.blade.php
@@ -1,1 +1,1 @@
-{!! link_to_route($link, $label, [$record->id], ['class' => 'btn btn-info @defaultButtonSize']) !!}
+{!! link_to_route($link, $label, [$record->id], ['data-button-type' => "duplicate", 'data-record-id' => $record->id, 'class' => 'btn btn-info @defaultButtonSize']) !!}
--- resources/views/grid/actions/edit.blade.php
+++ resources/views/grid/actions/edit.blade.php
@@ -1,1 +1,1 @@
-{!! link_to_route($link, $label, [$record->id], ['class' => 'btn btn-warning @defaultButtonSize']) !!}
+{!! link_to_route($link, $label, [$record->id], ['data-button-type' => "edit", 'data-record-id' => $record->id, 'class' => 'btn btn-warning @defaultButtonSize']) !!}
