b4244d3e5f48882510b8e98f82efa3f3681c4dcc
Reza Esmaili <bitbucket@dfox.info>
2020-01-28 21:53:13 +0100

    Fix vue bug with moustache tags in email template editor; add emailhelper

--- resources/views/backend/email_templates/form.blade.php
+++ resources/views/backend/email_templates/form.blade.php
@@ -1,22 +1,24 @@
-{!! form_start($form) !!}
-<div class="@boxWrapper box-primary">
-    <div class="@boxHeader with-border">
-        <h3 class="box-title">{{ trans('motor-backend::backend/global.base_info') }}</h3>
+<div v-pre>
+    {!! form_start($form) !!}
+    <div class="@boxWrapper box-primary">
+        <div class="@boxHeader with-border">
+            <h3 class="box-title">{{ trans('motor-backend::backend/global.base_info') }}</h3>
+        </div>
+        <div class="@boxBody">
+            {!! form_until($form, 'language_id') !!}
+        </div>
     </div>
-    <div class="@boxBody">
-        {!! form_until($form, 'language_id') !!}
-    </div>
-</div>
-<div class="@boxWrapper box-primary">
-    <div class="@boxHeader with-border">
-        <h3 class="box-title">{{ trans('motor-backend::backend/email_templates.template_info') }}</h3>
-    </div>
-    <div class="@boxBody">
-        {!! form_until($form, 'body_html') !!}
-    </div>
+    <div class="@boxWrapper box-primary">
+        <div class="@boxHeader with-border">
+            <h3 class="box-title">{{ trans('motor-backend::backend/email_templates.template_info') }}</h3>
+        </div>
+        <div class="@boxBody">
+            {!! form_until($form, 'body_html') !!}
+        </div>
 
-    <div class="@boxFooter">
-        {!! form_row($form->submit) !!}
+        <div class="@boxFooter">
+            {!! form_row($form->submit) !!}
+        </div>
     </div>
+    {!! form_end($form) !!}
 </div>
-{!! form_end($form) !!}
---
+++ src/Helpers/EmailHelper.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Motor\Backend\Helpers;
+
+use Illuminate\Support\Facades\Log;
+use Motor\Backend\Models\EmailTemplate;
+use Illuminate\Support\Facades\Mail;
+
+class EmailHelper
+{
+    public static function sendEmail($templateName, $headers = [], $data = [])
+    {
+        // Try to get email template
+        $template = EmailTemplate::where('name', $templateName)->first();
+        if (is_null($template)) {
+            Log::error('No template found for '.$templateName);
+            return false;
+        }
+
+        $body = view([ 'template' => nl2br($template->body_text) ], $data)->render();
+
+        try {
+            Mail::html($body, function ($message) use ($headers, $template, $body, $data) {
+                $message->setBody($body, 'text/html');
+                $message->from($template->default_sender_email, $template->default_sender_name);
+
+                if (isset($headers['to_email'])) {
+                    $message->to($headers['to_email'], $headers['to_name']);
+                } else {
+                    $message->to($template->default_recipient_email, $template->default_recipient_name);
+                }
+
+                if (isset($headers['cc_email'])) {
+                    $message->cc($headers['cc_email'], $headers['cc_name']);
+                }
+
+                if (isset($headers['bcc_email'])) {
+                    $message->bcc($headers['bcc_email'], $headers['bcc_name']);
+                }
+
+                $message->subject(html_entity_decode(view([ 'template' => $template->subject ], $data)->render()));
+
+            });
+        } catch (\Exception $e) {
+            Log::error($e->getMessage());
+
+            return false;
+        }
+
+        return true;
+    }
+
+}
