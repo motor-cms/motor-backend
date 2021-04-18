715192b404610c18221ed4815b625c2b86e20478
Reza Esmaili <bitbucket@dfox.info>
2021-04-18 23:47:14 +0200

    Add policies

---
+++ src/Policies/RolePolicy.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace Motor\Backend\Policies;
+
+use Illuminate\Auth\Access\HandlesAuthorization;
+use Motor\Backend\Models\Role;
+use Motor\Backend\Models\User;
+
+class RolePolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Perform pre-authorization checks.
+     *
+     * @param \Motor\Backend\Models\User $user
+     * @param string $ability
+     * @return void|bool
+     */
+    public function before(User $user, $ability)
+    {
+        if ($user->hasRole('SuperAdmin')) {
+            return true;
+        }
+    }
+
+    /**
+     * Determine whether the user can view any models.
+     *
+     * @param \Motor\Backend\Models\User $user
+     * @return mixed
+     */
+    public function viewAny(User $user)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     *
+     * @param \Motor\Backend\Models\User $user
+     * @param \Motor\Backend\Models\Role $role
+     * @return mixed
+     */
+    public function view(User $user, Role $role)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can create models.
+     *
+     * @param \Motor\Backend\Models\User $user
+     * @return mixed
+     */
+    public function create(User $user)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     *
+     * @param \Motor\Backend\Models\User $user
+     * @param \Motor\Backend\Models\Role $role
+     * @return mixed
+     */
+    public function update(User $user, Role $role)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     *
+     * @param \Motor\Backend\Models\User $user
+     * @param \Motor\Backend\Models\Role $role
+     * @return mixed
+     */
+    public function delete(User $user, Role $role)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     *
+     * @param \Motor\Backend\Models\User $user
+     * @param \Motor\Backend\Models\Role $role
+     * @return mixed
+     */
+    public function restore(User $user, Role $role)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     *
+     * @param \Motor\Backend\Models\User $user
+     * @param \Motor\Backend\Models\Role $role
+     * @return mixed
+     */
+    public function forceDelete(User $user, Role $role)
+    {
+        //
+    }
+}
