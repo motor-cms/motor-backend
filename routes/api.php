1d2c626f40f1ad279a36b5616fc4c9c81d4fe8d7
Reza Esmaili <bitbucket@dfox.info>
2022-07-01 15:10:18 +0200

    Refactor routes

--- routes/api.php
+++ routes/api.php
@@ -1,28 +1,43 @@
 <?php
 
+use Motor\Backend\Http\Controllers\Api\Auth\LoginController;
+use Motor\Backend\Http\Controllers\Api\CategoriesController;
+use Motor\Backend\Http\Controllers\Api\CategoryTreesController;
+use Motor\Backend\Http\Controllers\Api\ClientsController;
+use Motor\Backend\Http\Controllers\Api\ConfigVariablesController;
+use Motor\Backend\Http\Controllers\Api\EmailTemplatesController;
+use Motor\Backend\Http\Controllers\Api\LanguagesController;
+use Motor\Backend\Http\Controllers\Api\PermissionGroupsController;
+use Motor\Backend\Http\Controllers\Api\PermissionsController;
+use Motor\Backend\Http\Controllers\Api\ProfileEditController;
+use Motor\Backend\Http\Controllers\Api\RolesController;
+use Motor\Backend\Http\Controllers\Api\UsersController;
+
 Route::group([
     'middleware' => ['auth:api', 'bindings', 'permission'],
     'namespace'  => 'Motor\Backend\Http\Controllers\Api',
     'prefix'     => 'api',
     'as'         => 'api.',
 ], static function () {
-    Route::apiResource('users', 'UsersController');
-    Route::apiResource('clients', 'ClientsController');
-    Route::apiResource('languages', 'LanguagesController');
-    Route::apiResource('roles', 'RolesController');
-    Route::apiResource('permissions', 'PermissionsController');
-    Route::apiResource('permission_groups', 'PermissionGroupsController');
-    Route::apiResource('email_templates', 'EmailTemplatesController');
-    Route::apiResource('category_trees', 'CategoryTreesController', [
+    Route::apiResource('users', UsersController::class);
+    Route::apiResource('clients', ClientsController::class);
+    Route::apiResource('languages', LanguagesController::class);
+    Route::apiResource('roles', RolesController::class);
+    Route::apiResource('permissions', PermissionsController::class);
+    Route::apiResource('permission_groups', PermissionGroupsController::class);
+    Route::apiResource('email_templates', EmailTemplatesController::class);
+    Route::apiResource('category_trees', CategoryTreesController::class, [
         'parameters' => [
             'category_trees' => 'category',
         ],
     ]);
-    Route::apiResource('category_trees/{category_tree}/categories', 'CategoriesController');
+    Route::apiResource('category_trees/{category_tree}/categories', CategoriesController::class);
 
-    Route::get('profile', 'ProfileEditController@me')->name('profile.read');
-    Route::put('profile', 'ProfileEditController@update')->name('profile.update');
-    Route::apiResource('config_variables', 'ConfigVariablesController');
+    Route::get('profile', [ProfileEditController::class, 'me'])
+         ->name('profile.read');
+    Route::put('profile', [ProfileEditController::class, 'update'])
+         ->name('profile.update');
+    Route::apiResource('config_variables', ConfigVariablesController::class);
 });
 
 Route::group([
@@ -32,10 +47,14 @@ Route::group([
     'as'         => 'api.auth',
 
 ], static function ($router) {
-    Route::post('login', 'LoginController@login')->name('login');
-    Route::post('logout', 'LoginController@logout')->name('logout');
-    Route::post('refresh', 'LoginController@refresh')->name('refresh');
-    Route::post('me', 'LoginController@me')->name('me');
+    Route::post('login', [LoginController::class, 'login'])
+         ->name('login');
+    Route::post('logout', [LoginController::class, 'logout'])
+         ->name('logout');
+    Route::post('refresh', [LoginController::class, 'refresh'])
+         ->name('refresh');
+    Route::post('me', [LoginController::class, 'me'])
+         ->name('me');
 });
 
 Route::group([
@@ -44,5 +63,6 @@ Route::group([
     'prefix'     => 'ajax',
     'as'         => 'ajax.',
 ], static function () {
-    Route::get('categories', 'CategoriesController@index')->name('categories.index');
+    Route::get('categories', [CategoriesController::class, 'index'])
+         ->name('categories.index');
 });
--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,27 @@
 <?php
 
-//Route::get('/', function () {
-//    return redirect('/backend');
-//});
-//
-//Route::get('/home', function () {
-//    return redirect('/backend');
-//});
+use Motor\Backend\Http\Controllers\Auth\ChangePasswordController;
+use Motor\Backend\Http\Controllers\Backend\CategoriesController;
+use Motor\Backend\Http\Controllers\Backend\CategoryTreesController;
+use Motor\Backend\Http\Controllers\Backend\ClientsController;
+use Motor\Backend\Http\Controllers\Backend\ConfigVariablesController;
+use Motor\Backend\Http\Controllers\Backend\DashboardController;
+use Motor\Backend\Http\Controllers\Backend\EmailTemplatesController;
+use Motor\Backend\Http\Controllers\Backend\LanguagesController;
+use Motor\Backend\Http\Controllers\Backend\PermissionsController;
+use Motor\Backend\Http\Controllers\Backend\ProfileEditController;
+use Motor\Backend\Http\Controllers\Backend\RolesController;
+use Motor\Backend\Http\Controllers\Backend\UsersController;
 
 Route::group([
     'middleware' => ['web'],
     'namespace'  => 'Motor\Backend\Http\Controllers',
 ], static function () {
     Auth::routes(config('motor-backend.routes.auth', []));
-    Route::get('password/change', 'Auth\ChangePasswordController@showChangeForm')->name('auth.change-password.index');
-    Route::post('password/change', 'Auth\ChangePasswordController@change')->name('auth.change-password.store');
+    Route::get('password/change', [ChangePasswordController::class, 'showChangeForm'])
+         ->name('auth.change-password.index');
+    Route::post('password/change', [ChangePasswordController::class, 'change'])
+         ->name('auth.change-password.store');
 });
 
 Route::group([
@@ -31,60 +38,65 @@ Route::group([
         if (config('motor-backend.routes.dashboard')) {
             Route::get('/', [
                 'as'   => 'dashboard.index',
-                'uses' => 'DashboardController@show',
+                'uses' => [DashboardController::class, 'show'],
             ]);
-            Route::get('dashboard', 'DashboardController@show')->name('dashboard.index');
+            Route::get('dashboard', [DashboardController::class, 'show'])
+                 ->name('dashboard.index');
         }
 
         if (config('motor-backend.routes.users')) {
-            Route::resource('users', 'UsersController');
+            Route::resource('users', UsersController::class);
         }
 
         if (config('motor-backend.routes.roles')) {
-            Route::resource('roles', 'RolesController');
+            Route::resource('roles', RolesController::class);
         }
 
         if (config('motor-backend.routes.permissions')) {
-            Route::resource('permissions', 'PermissionsController');
+            Route::resource('permissions', PermissionsController::class);
         }
 
         if (config('motor-backend.routes.languages')) {
-            Route::resource('languages', 'LanguagesController');
+            Route::resource('languages', LanguagesController::class);
         }
 
         if (config('motor-backend.routes.clients')) {
-            Route::resource('clients', 'ClientsController');
+            Route::resource('clients', ClientsController::class);
         }
 
         if (config('motor-backend.routes.email_templates')) {
-            Route::resource('email_templates', 'EmailTemplatesController');
-            Route::get('email_templates/{email_template}/duplicate', 'EmailTemplatesController@duplicate')
+            Route::resource('email_templates', EmailTemplatesController::class);
+            Route::get('email_templates/{email_template}/duplicate', [EmailTemplatesController::class, 'duplicate'])
                  ->name('email_templates.duplicate');
         }
 
         if (config('motor-backend.routes.profile')) {
-            Route::get('profile/edit', 'ProfileEditController@edit')->name('profile.edit');
-            Route::patch('profile/edit', 'ProfileEditController@update')->name('profile.update');
+            Route::get('profile/edit', [ProfileEditController::class, 'edit'])
+                 ->name('profile.edit');
+            Route::patch('profile/edit', [ProfileEditController::class, 'update'])
+                 ->name('profile.update');
         }
 
-        Route::resource('categories', 'CategoriesController', [
+        Route::resource('categories', CategoriesController::class, [
             'except' => [
                 'index',
                 'create',
             ],
         ]);
 
-        Route::get('categories/{category}', 'CategoriesController@index')->name('categories.index');
-        Route::get('categories/{category}/create', 'CategoriesController@create')->name('categories.create');
+        Route::get('categories/{category}', [CategoriesController::class, 'index'])
+             ->name('categories.index');
+        Route::get('categories/{category}/create', [CategoriesController::class, 'create'])
+             ->name('categories.create');
 
-        Route::resource('category_trees', 'CategoryTreesController', [
+        Route::resource('category_trees', CategoryTreesController::class, [
             'parameters' => [
                 'category_trees' => 'category',
             ],
         ]);
 
-        Route::resource('config_variables', 'ConfigVariablesController');
-        Route::get('config_variables/{config_variable}/duplicate', 'ConfigVariablesController@duplicate')
+        Route::resource('config_variables', ConfigVariablesController::class);
+        Route::get('config_variables/{config_variable}/duplicate', [ConfigVariablesController::class, 'duplicate'])
              ->name('config_variables.duplicate');
     });
 });
