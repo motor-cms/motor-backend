aa9c6ec0be17dc04fdeee3e4d097d12d207a04c5
Reza Esmaili <bitbucket@dfox.info>
2023-04-06 23:00:46 +0200

    Remove image stuff from normal file associations

--- resources/assets/js/motor-backend/components/FileAssociation.vue
+++ resources/assets/js/motor-backend/components/FileAssociation.vue
@@ -5,24 +5,6 @@
                 {{$t('motor-media.backend.files.drop_file_here')}}
             </div>
             <template v-for="(file, index) in files">
-                <!--                <img v-if="isImage(file)" class="card-img-top" :src="file.file.preview">-->
-                <cropper
-                        ref="cropper"
-                        v-if="isImage(file)"
-                        class="card-img-top"
-                        :src="file.file.conversions.preview"
-                        classname="cropper"
-                        :stencilProps="stencilProps"
-                        :minWidth="minWidth"
-                        :minHeight="minHeight"
-                        @change="changeImage"
-                ></cropper>
-                <div class="btn-group">
-                    <a class="btn btn-light" @click="changeAspect(16/9)">16:9</a>
-                    <a class="btn btn-light" @click="changeAspect(4/3)">4:3</a>
-                    <a class="btn btn-light" @click="changeAspect(1)">1:1</a>
-                    <a class="btn btn-light" @click="changeAspect(0)">Free</a>
-                </div>
                 <div class="card-body" data-toggle="tooltip" data-placement="top" :title="file.description">
                     <button class="btn btn-danger float-right" type="button" @click="deleteFile(file)"><i
                             class="fa fa-trash-alt"></i>
